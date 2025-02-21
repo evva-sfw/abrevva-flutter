21f2261395c40e32cefb30ffd9d9ddd940ab6868
mhochsto <116495532+mhochsto@users.noreply.github.com>
2025-02-21 12:58:14 +0100

    feat: ios lint

--- example/ios/Runner/AppDelegate.swift
+++ example/ios/Runner/AppDelegate.swift
@@ -1,15 +1,14 @@
-import UIKit
-import Flutter
 import CoreBluetooth
+import Flutter
+import UIKit
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {    
+@objc class AppDelegate: FlutterAppDelegate {
     override func application(
-    
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
 }
