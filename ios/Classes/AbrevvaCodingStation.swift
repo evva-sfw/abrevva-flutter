5b31a362a2c607cdbcc9ff04874b091a925ea127
mhochsto <116495532+mhochsto@users.noreply.github.com>
2025-02-27 09:19:32 +0100

    feat: added swift implementation for coding station ; minor tweaks in AbrevvaPlugin & crypto Files

---
+++ ios/Classes/AbrevvaCodingStation.swift
@@ -0,0 +1,80 @@
+import Foundation
+import AbrevvaSDK
+import CryptoSwift
+import Flutter
+
+public class AbrevvaCodingStation: NSObject, FlutterPlugin {
+
+    let codingStation = CodingStation()
+    var mqttConnectionOptions: MqttConnectionOptions?
+
+    public static func register(with registrar: FlutterPluginRegistrar) {}
+
+    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        switch call.method {
+        case "registerMqttConfigForXS":
+            registerMqttConfigForXS(call, result: result)
+        case "connect":
+            connect(call, result: result)
+        case "write":
+            write(call, result: result)
+        case "disconnect":
+            disconnect(call, result: result)
+        default:
+            result(FlutterMethodNotImplemented)
+        }
+    }
+
+    @objc
+    private func registerMqttConfigForXS(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        guard let args = call.arguments as? [String: Any] else {
+            return result(FlutterError(code: "Failed to convert NSDictionary to Swift dictionary", message: nil, details: nil))
+        }
+        let url = URL(string: args["url"] as? String ?? "")!
+        let clientId = args["clientId"] as? String ?? ""
+        let username = args["username"] as? String ?? ""
+        let password = args["password"] as? String ?? ""
+
+        Task {
+            do {
+                mqttConnectionOptions = try await AuthManager.getMqttConfigForXS(url: url, clientId: clientId, username: username, password: password)
+                result(true)
+            } catch {
+                result(FlutterError(code: "getMqttConfigForXS(): \(error)", message: nil, details: nil))
+            }
+        }
+    }
+
+    @objc
+    private func connect(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        if self.mqttConnectionOptions == nil {
+            return result(FlutterError(code: "No MqttConfig set. Call registerMqttConfigForXS() first.", message: nil, details: nil))
+        }
+        Task {
+            do {
+                try await codingStation.connect(mqttConnectionOptions!)
+                result(true)
+            } catch {
+                result(FlutterError(code: "connect(): \(error)", message: nil, details: nil))
+            }
+        }
+    }
+
+    @objc
+    private func write(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        Task {
+            do {
+                try await codingStation.write()
+                result(true)
+            } catch {
+                result(FlutterError(code: "write(): \(error)", message: nil, details: nil))
+            }
+        }
+    }
+
+    @objc
+    private func disconnect(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        codingStation.disconnect()
+        result(true)
+    }
+}
--- ios/Classes/AbrevvaPlugin.swift
+++ ios/Classes/AbrevvaPlugin.swift
@@ -3,23 +3,39 @@ import UIKit
 
 public class AbrevvaPlugin: NSObject, FlutterPlugin {
     public static func register(with registrar: FlutterPluginRegistrar) {
+
+        /// Register `AbrevvaCrypto` Method Channel
+        let channelCrypto = FlutterMethodChannel(name: "AbrevvaCrypto", binaryMessenger: registrar.messenger())
+        registrar.addMethodCallDelegate(AbrevvaCrypto(), channel: channelCrypto)
+
+        /// Register `AbrevvaBle` Method Channel
+        /// and create individual EventChannels for all Memberfunctions that require an EventChannel
         let channelBle = FlutterMethodChannel(name: "AbrevvaBle", binaryMessenger: registrar.messenger())
         let instanceBle = AbrevvaBle()
         registrar.addMethodCallDelegate(instanceBle, channel: channelBle)
 
-        let channelCrypto = FlutterMethodChannel(name: "AbrevvaCrypto", binaryMessenger: registrar.messenger())
-        registrar.addMethodCallDelegate(AbrevvaCrypto(), channel: channelCrypto)
-
-        let bleEventChannel = FlutterEventChannel(name: "AbrevvaBleEvents", binaryMessenger: registrar.messenger())
-        bleEventChannel.setStreamHandler(instanceBle.streamHandler)
-
-        let connectEventChannel = FlutterEventChannel(name: "connectEventChannel", binaryMessenger: registrar.messenger())
-        let startScanEventChannel = FlutterEventChannel(name: "startScanEventChannel", binaryMessenger: registrar.messenger())
-        let startNotificationsEventChannel = FlutterEventChannel(name: "startNotificationsEventChannel", binaryMessenger: registrar.messenger())
-        let startEnabledNotificationsEventChannel = FlutterEventChannel(name: "startEnabledNotificationsEventChannel", binaryMessenger: registrar.messenger())
-        startScanEventChannel.setStreamHandler(instanceBle.connectStreamHandler)
+        let connectEventChannel = FlutterEventChannel(
+            name: "connectEventChannel", binaryMessenger: registrar.messenger()
+        )
+        let startScanEventChannel = FlutterEventChannel(
+            name: "startScanEventChannel", binaryMessenger: registrar.messenger()
+        )
+        let startNotificationsEventChannel = FlutterEventChannel(
+            name: "startNotificationsEventChannel", binaryMessenger: registrar.messenger()
+        )
+        let startEnabledNotificationsEventChannel = FlutterEventChannel(
+            name: "startEnabledNotificationsEventChannel", binaryMessenger: registrar.messenger()
+        )
+        connectEventChannel.setStreamHandler(instanceBle.connectStreamHandler)
         startScanEventChannel.setStreamHandler(instanceBle.startScanStreamHandler)
         startNotificationsEventChannel.setStreamHandler(instanceBle.startNotificationsStreamHandler)
         startEnabledNotificationsEventChannel.setStreamHandler(instanceBle.startEnabledNotificationsEventChannel)
+
+        /// Register `AbrevvaCodingStation` Method Channel
+        let channelCodingStation = FlutterMethodChannel(
+            name: "AbrevvaCodingStation", binaryMessenger: registrar.messenger()
+        )
+        registrar.addMethodCallDelegate(AbrevvaCodingStation(), channel: channelCodingStation)
+
     }
 }
